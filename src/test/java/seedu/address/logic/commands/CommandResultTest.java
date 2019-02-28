40379f8bac9f06c37e5a0137a5db13979655497a
exactlyprecise <ngweilele@gmail.com>
2019-02-28 19:09:55 +0800

    CommandResultTest.java: Add extra hashcode testcase

--- src/test/java/seedu/address/logic/commands/CommandResultTest.java
+++ src/test/java/seedu/address/logic/commands/CommandResultTest.java
@@ -42,8 +42,8 @@ public class CommandResultTest {
     public void hashcode() {
         CommandResult commandResult = new CommandResult("feedback");
 
-        // same values -> returns same has
-        // ), new CommandResult("feedback").hashCode());
+        // same values -> returns same hashcode
+        assertEquals(commandResult.hashCode(), new CommandResult("feedback").hashCode());
 
         // different feedbackToUser value -> returns different hashcode
         assertNotEquals(commandResult.hashCode(), new CommandResult("different").hashCode());
