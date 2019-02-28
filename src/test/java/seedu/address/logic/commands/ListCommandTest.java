211f8fffc19d08640e9200e0ffb0f76ef7f87a14
exactlyprecise <ngweilele@gmail.com>
2019-02-28 19:08:56 +0800

    Change line to fit checkstyle requirements

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -2,9 +2,9 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.logic.commands.ListCommand.MESSAGE_SUCCESS;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-import static seedu.address.logic.commands.ListCommand.MESSAGE_SUCCESS;
 
 import org.junit.Before;
 import org.junit.Test;
