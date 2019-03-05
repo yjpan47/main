ea5eee64042dd95359da5251bc1f8e560580a9a5
mechome <32713221+mechome@users.noreply.github.com>
2019-03-05 22:01:53 +0800

    address/ui: edit ui classes to suit new attributes and new name

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -19,7 +19,7 @@ public class PersonCard extends UiPart<Region> {
      * As a consequence, UI elements' variable names cannot be set to such keywords
      * or an exception will be thrown by JavaFX during runtime.
      *
-     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on PersonnelDatabase level 4</a>
      */
 
     public final Person person;
@@ -27,15 +27,19 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private HBox cardPane;
     @FXML
+    private Label nric;
+    @FXML
+    private Label company;
+    @FXML
+    private Label section;
+    @FXML
+    private Label rank;
+    @FXML
     private Label name;
     @FXML
     private Label id;
     @FXML
     private Label phone;
-    @FXML
-    private Label address;
-    @FXML
-    private Label email;
     @FXML
     private FlowPane tags;
 
@@ -43,10 +47,12 @@ public class PersonCard extends UiPart<Region> {
         super(FXML);
         this.person = person;
         id.setText(displayedIndex + ". ");
+        nric.setText(person.getNric().value);
+        company.setText(person.getCompany().value);
+        section.setText(person.getSection().value);
+        rank.setText(person.getRank().value);
         name.setText(person.getName().fullName);
         phone.setText(person.getPhone().value);
-        address.setText(person.getAddress().value);
-        email.setText(person.getEmail().value);
         person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
 
