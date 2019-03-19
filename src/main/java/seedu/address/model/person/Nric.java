f994caca30cf10d613c4660020fdd79f07beba38
dreampursuer95 <36196473+dreampursuer95@users.noreply.github.com>
2019-03-19 14:48:27 +0800

    Nric.java fix

--- src/main/java/seedu/address/model/person/Nric.java
+++ src/main/java/seedu/address/model/person/Nric.java
@@ -4,31 +4,35 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Person's NRIC in the duty planner.
- * Guarantees: immutable; is valid as declared in {@link #isValidNric(String)}
+ * Represents a Person's NRIC in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidNRIC(String)}
  */
-public class Nric {
+public class NRIC {
 
-    public static final String MESSAGE_CONSTRAINTS = "NRICs should be of the format [S/T/F/G][7 digits][A-Z]";
-    public static final String VALIDATION_REGEX = "[STFG]\\d{7}[A-Z]";
+    public static final String MESSAGE_CONSTRAINTS = "NRIC is the IC number of the soldier";
+
+
+
+    public static final String VALIDATION_REGEX = "\\s\\d{7}\\s";
+
 
     public final String value;
 
     /**
-     * Constructs an {@code Nric}.
+     * Constructs an {@code NRIC}.
      *
      * @param nric A valid nric.
      */
-    public Nric(String nric) {
+    public NRIC(String nric) {
         requireNonNull(nric);
-        checkArgument(isValidNric(nric), MESSAGE_CONSTRAINTS);
+        checkArgument(isValidNRIC(nric), MESSAGE_CONSTRAINTS);
         value = nric;
     }
 
     /**
-     * Returns true if a given string is a valid nric.
+     * Returns true if a given string is a valid NRIC.
      */
-    public static boolean isValidNric(String test) {
+    public static boolean isValidNRIC(String test) {
         return test.matches(VALIDATION_REGEX);
     }
 
@@ -40,8 +44,8 @@ public class Nric {
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof Nric // instanceof handles nulls
-                && value.equals(((Nric) other).value)); // state check
+                || (other instanceof NRIC // instanceof handles nulls
+                && value.equals(((NRIC) other).value)); // state check
     }
 
     @Override
