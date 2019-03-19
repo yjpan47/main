a9d5aa32fc8feaac6053e26f546b205ca87d145d
valatheking <valavan.rajan@gmail.com>
2019-03-19 17:03:44 +0800

    Travis Edit

--- src/main/java/seedu/address/logic/commands/AdminCommand.java
+++ src/main/java/seedu/address/logic/commands/AdminCommand.java
@@ -1,11 +1,11 @@
 package seedu.address.logic.commands;
-/**
- * Admin command interface
- */
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-
+/**
+ * Admin command interface
+ */
 public interface AdminCommand {
     public CommandResult executeAdmin(Model model, CommandHistory commandHistory) throws CommandException;
 }
