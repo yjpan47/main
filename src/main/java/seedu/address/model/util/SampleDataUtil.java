db774fa1d439af81160f309cbd629f92d1123f4e
valatheking <valavan.rajan@gmail.com>
2019-03-19 16:48:01 +0800

    Travis Edit

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -16,7 +16,6 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
-
 //            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
 //                new Address("Blk 30 Geylang Street 29, #06-40"),
 //                getTagSet("friends")),
@@ -33,8 +32,7 @@ public class SampleDataUtil {
 //                new Address("Blk 47 Tampines Street 20, #17-35"),
 //                getTagSet("classmates")),
 //            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-//                new Address("Blk 45 Aljunied Street 85, #11-31"),
-//                getTagSet("colleagues"))
+//                new Address("Blk 45 Aljunied Street 85, #11-31"),getTagSet("colleagues"))
         };
     }
 
