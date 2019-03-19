68785b40ff7f24a7dbb983d1519d8c20e741f499
valatheking <valavan.rajan@gmail.com>
2019-03-19 16:47:10 +0800

    Travis Edit added javadoc

--- src/main/java/seedu/address/model/calendar/DutyMonth.java
+++ src/main/java/seedu/address/model/calendar/DutyMonth.java
@@ -18,8 +18,7 @@ public class DutyMonth {
     private List<DutyDate> dates;
 
     public DutyMonth(int monthIndex, int firstDayIndex) {
-        if (monthIndex >= 1 && monthIndex <= 12
-                && firstDayIndex >= 0 && firstDayIndex <= 7)  {
+        if (monthIndex >= 1 && monthIndex <= 12 && firstDayIndex >= 0 && firstDayIndex <= 7)  {
             this.monthIndex = monthIndex;
             this.month = this.getMonth();
             this.firstDayIndex = firstDayIndex;
@@ -29,26 +28,31 @@ public class DutyMonth {
             throw new InputMismatchException("Invalid Month Index or first Day Index");
         }
     }
-
+    /**
+     * Gets current month
+     */
     public String getMonth() {
-        String[] months = {"January", "February", "March",
-                "April", "May", "June", "July", "August",
-                "September", "October", "November", "December"};
+        String[] months = {"January", "February", "March", "April", "May", "June", "July"
+                ,"August", "September", "October", "November", "December"};
         return months[this.monthIndex - 1];
     }
-
+    /**
+     * Returns current month
+     */
     public boolean isCurrentMonth() {
         Calendar cal = Calendar.getInstance();
         int currentMonth = cal.get(Calendar.MONTH);
         return (this.monthIndex == currentMonth);
     }
-
+    /**
+     * Gets the number of days in the current month
+     */
     private int getNumOfDays() {
         if (Arrays.asList(1, 3, 5, 7, 8, 10, 12).contains(this.monthIndex)) {
             return 31;
         } else if (Arrays.asList(4, 6, 9, 11).contains(this.monthIndex)) {
             return 30;
-        } else if (this.monthIndex == 2){
+        } else if (this.monthIndex == 2) {
             return 28;
         } else {
             throw new InputMismatchException("Invalid Month Index.");
