d794a63ea0ae90dd054f094f3d4bd185975f450d
valatheking <valavan.rajan@gmail.com>
2019-03-19 12:40:21 +0800

    Travis Edit

--- src/main/java/seedu/address/commons/core/UserType.java
+++ src/main/java/seedu/address/commons/core/UserType.java
@@ -1,5 +1,9 @@
 package seedu.address.commons.core;
-
+/**
+ *  Usertypes that can be used
+ *  <li>{@link #GENERAL}</li>
+ *  <li>{@link #ADMIN}</li>
+ */
 public enum UserType {
     GENERAL,
     ADMIN;
