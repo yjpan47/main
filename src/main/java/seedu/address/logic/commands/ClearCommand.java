34cec596753d1f18721d6250658fa7788bb15076
valatheking <valavan.rajan@gmail.com>
2019-03-19 12:40:44 +0800

    Travis Edit

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -5,8 +5,8 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.PersonnelDatabase;
 import seedu.address.model.Model;
+import seedu.address.model.PersonnelDatabase;
 
 /**
  * Clears the address book.
