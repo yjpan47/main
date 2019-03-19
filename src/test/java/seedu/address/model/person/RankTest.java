bdc86cb60b15343be518ea798abbc76129bc1d72
valatheking <valavan.rajan@gmail.com>
2019-03-20 01:37:23 +0800

    Travis Edit

--- src/test/java/seedu/address/model/person/RankTest.java
+++ src/test/java/seedu/address/model/person/RankTest.java
@@ -1,10 +1,10 @@
 package seedu.address.model.person;
 
-import org.junit.Test;
-import seedu.address.testutil.Assert;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import seedu.address.testutil.Assert;
 
 public class RankTest {
 
