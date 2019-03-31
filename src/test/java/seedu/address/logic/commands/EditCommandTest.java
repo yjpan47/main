3e4760d66d45369141039f576979fdfd1c58bb26
dreampursuer95 <36196473+dreampursuer95@users.noreply.github.com>
2019-04-01 00:55:16 +0800

    Checkstyle

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
