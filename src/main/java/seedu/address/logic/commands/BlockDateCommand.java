cfe7cfc127b353158ae814c3b611d5feee1ad46f
valatheking <valavan.rajan@gmail.com>
2019-04-09 22:57:15 +0800

    Update Block Command

--- src/main/java/seedu/address/logic/commands/BlockDateCommand.java
+++ src/main/java/seedu/address/logic/commands/BlockDateCommand.java
@@ -12,7 +12,7 @@ import seedu.address.model.person.Person;
  */
 public class BlockDateCommand extends Command {
 
-    public static final String COMMAND_WORD = "bd";
+    public static final String COMMAND_WORD = "block";
     public static final String MESSAGE_USAGE = COMMAND_WORD + " Blocks out certain duty dates for the upcoming "
             + "month for the current person using.\n"
             + "Parameters: Dates in numbers for the next month\n"
