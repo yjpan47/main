43b07f1765e4e36a21e57891d81cf58a92b182c1
valatheking <valavan.rajan@gmail.com>
2019-04-13 19:31:56 +0800

    Updated Block Command Test

--- src/test/java/seedu/address/logic/commands/BlockDateCommandTest.java
+++ src/test/java/seedu/address/logic/commands/BlockDateCommandTest.java
@@ -1,8 +1,13 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-//import static seedu.address.logic.commands.BlockDateCommand.MESSAGE_TOO_MANY_BLOCKED_DATES;
+import static seedu.address.logic.commands.BlockDateCommand.MESSAGE_BLOCK_DATES_SUCCESS;
+
+import static seedu.address.logic.commands.BlockDateCommand.MESSAGE_TOO_MANY_BLOCKED_DATES;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailureGeneral;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccessGeneral;
 import static seedu.address.testutil.TypicalPersons.getTypicalPersonnelDatabase;
+import static seedu.address.testutil.TypicalPersons.GENERAL_DAN_USERNAME;
 
 import java.util.ArrayList;
 //import java.util.Arrays;
@@ -17,6 +22,7 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
+
 /**
  * Contains integration tests (interaction with the Model) and unit tests for BlockDateCommand.
  */
@@ -24,12 +30,24 @@ public class BlockDateCommandTest {
 
     private Model model = new ModelManager(getTypicalPersonnelDatabase(), new UserPrefs());
 
+
     @Test
     public void execute() {
         Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
         List<Integer> blockedDates = new ArrayList<>();
         Collections.addAll(blockedDates, arr);
-        assertCommandFailure(new BlockDateCommand(blockedDates, "Admin"), model, new CommandHistory(),
-                Messages.MESSAGE_NO_AUTHORITY_PARSE);
+        assertCommandFailureGeneral(new BlockDateCommand(blockedDates, GENERAL_DAN_USERNAME), model,
+                new CommandHistory(), MESSAGE_TOO_MANY_BLOCKED_DATES);
+
+        assertCommandFailure(new BlockDateCommand(blockedDates, GENERAL_DAN_USERNAME), model,
+                new CommandHistory(), Messages.MESSAGE_NO_AUTHORITY_PARSE);
+        blockedDates.remove(1);
+        blockedDates.remove(1);
+        blockedDates.remove(1);
+        blockedDates.remove(1);
+        blockedDates.remove(1);
+        blockedDates.remove(1);
+        assertCommandSuccessGeneral(new BlockDateCommand(blockedDates, GENERAL_DAN_USERNAME), model,
+                new CommandHistory(), MESSAGE_BLOCK_DATES_SUCCESS, model);
     }
 }
