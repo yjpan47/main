6c360bc816198e0b9d3c427060ed30e5d07a5504
valatheking <valavan.rajan@gmail.com>
2019-04-14 02:43:08 +0800

    Checkstyle Edits

--- src/test/java/seedu/address/logic/commands/ConfirmScheduleCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ConfirmScheduleCommandTest.java
@@ -1,14 +1,5 @@
 package seedu.address.logic.commands;
 
-import org.junit.Test;
-
-import seedu.address.commons.core.Messages;
-//import seedu.address.commons.util.DateUtil;
-import seedu.address.logic.CommandHistory;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
-
 //import static seedu.address.logic.commands.BlockDateCommand.MESSAGE_DUTY_CONFIRMED;
 //import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailureGeneral;
@@ -19,13 +10,25 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailureG
 //import static seedu.address.testutil.TypicalPersons.GENERAL_DAN_USERNAME;
 import static seedu.address.testutil.TypicalPersons.getTypicalPersonnelDatabase;
 
+import org.junit.Test;
+
+import seedu.address.commons.core.Messages;
+//import seedu.address.commons.util.DateUtil;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+
+
 public class ConfirmScheduleCommandTest {
 
     private Model model = new ModelManager(getTypicalPersonnelDatabase(), new UserPrefs());
 
     @Test
-    public void execute(){
+    public void execute() {
 
         assertCommandFailureGeneral(new ScheduleCommand(), model, new CommandHistory(), Messages.MESSAGE_NO_AUTHORITY);
+        
     }
 }
