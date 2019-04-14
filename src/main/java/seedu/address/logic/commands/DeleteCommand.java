f878a608520cd4868280d9ebd08fe1b9dbf219be
dreampursuer95 <36196473+dreampursuer95@users.noreply.github.com>
2019-04-14 13:04:43 +0800

    Checkstyle

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -9,7 +9,6 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.duty.Duty;
 import seedu.address.model.duty.DutyMonth;
 import seedu.address.model.duty.DutyStorage;
 import seedu.address.model.person.Person;
