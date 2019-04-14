d8f2cc3e33253de111dd2087b2edbae3d09810ac
mechome <32713221+mechome@users.noreply.github.com>
2019-04-14 10:09:07 +0800

    edit tests

--- src/test/java/seedu/address/logic/commands/ViewNextCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewNextCommandTest.java
@@ -1,14 +1,14 @@
-package seedu.address.logic;
+package seedu.address.logic.commands;
 
-import static seedu.address.logic.ViewNextCommand.MESSAGE_SUCCESS;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccessGeneral;
+import static seedu.address.logic.commands.ViewNextCommand.MESSAGE_SUCCESS;
 import static seedu.address.testutil.TypicalPersons.getTypicalPersonnelDatabase;
 
 import org.junit.Test;
 
 import seedu.address.commons.core.UiCommandInteraction;
-import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
