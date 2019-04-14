9f3483b71b725b2e50dab6797b544addabac2006
mechome <32713221+mechome@users.noreply.github.com>
2019-04-15 00:53:38 +0800

    Add more tests for AcceptSwapCommand and ViewSwapsCommand

--- src/test/java/seedu/address/logic/commands/AcceptSwapCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AcceptSwapCommandTest.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccessGeneral;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.ALICE_NRIC;
@@ -10,16 +9,18 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.junit.Test;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.duty.Duty;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.person.Person;
 import seedu.address.model.UserPrefs;
 
 /**
- * Contains integration tests (interaction with the Model) and unit tests for SwapCommand.
+ * Contains integration tests (interaction with the Model) and unit tests for AcceptSwapCommand.
  */
-public class SwapCommandTest {
+public class AcceptSwapCommandTest {
 
     private Model model = new ModelManager(getTypicalPersonnelDatabaseWithDuties(), new UserPrefs());
 
@@ -28,24 +29,33 @@ public class SwapCommandTest {
         List<Duty> duties = model.getDutyCalendar().getNextMonth().getScheduledDuties();
         int nextMonthIndex = model.getDutyCalendar().getNextMonth().getMonthIndex() + 1;
         int nextMonthYear = model.getDutyCalendar().getNextMonth().getYear();
+        int allocatedDayIndex = 0;
         LocalDate allocatedDate = null;
         LocalDate requestedDate = null;
+        Person accepter  = null;
         for (Duty duty : duties) {
             if (duty.getPersons().contains(ALICE)) {
-                allocatedDate = LocalDate.of(nextMonthYear, nextMonthIndex, duty.getDayIndex());
+                allocatedDayIndex = duty.getDayIndex();
+                allocatedDate = LocalDate.of(nextMonthYear, nextMonthIndex, allocatedDayIndex);
                 break;
             }
         }
         for (Duty duty : duties) {
             if (!duty.getPersons().contains(ALICE)) {
                 requestedDate = LocalDate.of(nextMonthYear, nextMonthIndex, duty.getDayIndex());
+                for (Person person : duty.getPersons()) {
+                    if (!duties.get(allocatedDayIndex - 1).contains(person)) {
+                        accepter = person;
+                        break;
+                    }
+                }
                 break;
             }
         }
-
-        assertCommandFailure(new SwapCommand(requestedDate, allocatedDate, ALICE_NRIC), model, new CommandHistory(),
-                SwapCommand.MESSAGE_INVALID_DAY_ALLOCATED);
+        CommandHistory ch = new CommandHistory();
         assertCommandSuccessGeneral(new SwapCommand(allocatedDate, requestedDate, ALICE_NRIC), model,
-                new CommandHistory(), new CommandResult(SwapCommand.MESSAGE_SUCCESS), model);
+                ch, new CommandResult(SwapCommand.MESSAGE_SUCCESS), model);
+        assertCommandSuccessGeneral(new AcceptSwapCommand(accepter.getNric().toString(), Index.fromOneBased(1)), model,
+                ch, new CommandResult(AcceptSwapCommand.MESSAGE_SUCCESS), model);
     }
 }
